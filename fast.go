8cd77875c26b440247f2c4aecc6ac5577887042e
Jeremy Holland <jeremy@jeremypholland.com>
2014-06-12 10:06:05 -0500

    Adding mad commentary because this shit can get confusing

--- fast.go
+++ fast.go
@@ -39,7 +39,7 @@ type keypoint struct {
 func NewFast(oct *octave, thresh uint8) *fast {
   f := new(fast)
   f.image = oct.image
-  f.thresh = NewThreshTable(f.image.Bounds().Max.X, thresh)
+  f.thresh = NewThreshTable(thresh)
   f.keypoints = make([]*keypoint, 0, 2)
   f.findKeypoints()
   return f
@@ -48,7 +48,7 @@ func NewFast(oct *octave, thresh uint8) *fast {
 func NewFastFromGray(gray *image.Gray, thresh uint8) *fast {
   f := new(fast)
   f.image = gray
-  f.thresh = NewThreshTable(f.image.Bounds().Max.X, thresh)
+  f.thresh = NewThreshTable(thresh)
   f.keypoints = make([]*keypoint, 0, 2)
   f.findKeypoints()
   return f
@@ -62,11 +62,17 @@ func (f *fast) findKeypoints() {
       // tree and - frankly - ain't nobody got time for dat.
       v := f.image.At(i, j).(color.Gray).Y
       tab := f.thresh.table[-v + 255:]
+
+      // Compare pixels at top and bottom. If neither differ by the threshold,
+      // disregard the rest and continue to the next pixel
       d := tab[f.image.At(i + pattern[0][0], j + pattern[0][1]).(color.Gray).Y] |
            tab[f.image.At(i + pattern[8][0], j + pattern[8][1]).(color.Gray).Y]
       if d == 0 {
         continue
       }
+
+      // Compare pixels at left, right, and the 45-degree diagonals. If none differ
+      // by the threshold, disregard the rest and continue to the next pixel
       d &= tab[f.image.At(i + pattern[2][0], j + pattern[2][1]).(color.Gray).Y] |
            tab[f.image.At(i + pattern[10][0], j + pattern[10][1]).(color.Gray).Y]
       d &= tab[f.image.At(i + pattern[4][0], j + pattern[4][1]).(color.Gray).Y] |
@@ -76,6 +82,8 @@ func (f *fast) findKeypoints() {
       if d == 0 {
         continue
       }
+
+      // Compare all remaining pixels of the Bresenham's circle.
       d &= tab[f.image.At(i + pattern[1][0], j + pattern[1][1]).(color.Gray).Y] |
            tab[f.image.At(i + pattern[9][0], j + pattern[9][1]).(color.Gray).Y]
       d &= tab[f.image.At(i + pattern[3][0], j + pattern[3][1]).(color.Gray).Y] |
@@ -84,6 +92,8 @@ func (f *fast) findKeypoints() {
            tab[f.image.At(i + pattern[13][0], j + pattern[13][1]).(color.Gray).Y]
       d &= tab[f.image.At(i + pattern[7][0], j + pattern[7][1]).(color.Gray).Y] |
            tab[f.image.At(i + pattern[15][0], j + pattern[15][1]).(color.Gray).Y]
+
+      // scan for contiguity of differing pixels
       if !f.scanContiguous(1, d, int(v - f.thresh.threshold), i, j) {
         f.scanContiguous(2, d, int(v + f.thresh.threshold), i, j)
       }
@@ -92,12 +102,19 @@ func (f *fast) findKeypoints() {
 }
 
 func (f *fast) scanContiguous(comp, d uint8, thr, x, y int) (bool) {
+  // Ensure that the comparison is still valid
   if d & comp != 0 {
     count := 0
+    // Loop over the pattern 1.5 times to ensure comparison of all
+    // possible contiguities
     for i := 0; i < 25; i++ {
       pv := f.image.At(x + pattern[i % 16][0], y + pattern[i % 16][1]).(color.Gray).Y
+      // If this pixel beats the threshold, bump the count. Otherwise,
+      // reset it.
       if int(pv) < thr {
         count++
+        // If there are at least 9 contiguous pixels meeting the
+        // difference criteria, we have a keypoint. Congratulations!
         if count > 8 {
           k := new(keypoint)
           k.point = new(image.Point)
@@ -115,6 +132,8 @@ func (f *fast) scanContiguous(comp, d uint8, thr, x, y int) (bool) {
   return false
 }
 
+// Returns the original image (in grayscale RGB) with the keypoints
+// rendered as solid green pixels.
 func (f *fast) RenderKeypoints() *image.RGBA {
   kpcolor := color.RGBA{0,255,0,255}
   img := ConvertToColor(f.image)
@@ -125,6 +144,8 @@ func (f *fast) RenderKeypoints() *image.RGBA {
   return img
 }
 
+// Returns a black image (in RGB) with the keypoints rendered as
+// solid green pixels.
 func (f *fast) RenderKeypointsOnly() *image.RGBA {
   black := color.RGBA{0,0,0,255}
   kpcolor := color.RGBA{0,255,0,255}
--- thresh_table.go
+++ thresh_table.go
@@ -5,7 +5,14 @@ type threshTable struct {
   table []uint8
 }
 
-func NewThreshTable(cols int, threshold uint8) *threshTable {
+// Constructs the table for binary comparison based on
+// the threshold value. Once sliced to the nexus pixel's
+// value, the resulting slice value at the query pixel's
+// value will return 1 if the query is less than the
+// nexus by at least the threshold, 2 if the query is
+// greater than the nexus by at least the threshold, and
+// 0 otherwise.
+func NewThreshTable(threshold uint8) *threshTable {
   tab := new(threshTable)
   tab.threshold = threshold
   tab.table = make([]uint8, 512)
